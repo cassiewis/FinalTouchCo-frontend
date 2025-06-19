13558a0018f5555b35053d6163ab95e18fe7a057
Cassie Coleman <cassiecoleman@Cassies-MacBook-Pro.local>
2025-06-19 10:04:27 -0600

    update constants

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,4 +1,5 @@
 export const BUFFER_DAYS = 3;
 export const RESERVATION_LENGTH = 5;
+export const EMAIL = "finaltouchco.info@gmail.com"; // Number of days for reservation
 export const BACKEND_URL = "https://finaltouchco-backend.onrender.com"
 // export const BACKEND_URL = "http://192.168.0.46:8080"
