618e8ef08803dddafbec9f5cd119fea9b1832530
Cassie Coleman <cassiewis24@gmail.com>
2025-07-15 00:23:05 -0700

    update constants

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -7,8 +7,8 @@ export const SEND_INVOICE_DAYS = 45; // Days before the date to send invoice
 export const PAYMENT_DUE_DAYS = 30; // Days before the date to send deposit
 export const DAILY_LATE_FEE = 50; // Daily late fee in dollars
 export const TAX_PERCENTAGE = 0.06; // 6% tax rate
-// export const BACKEND_URL = "https://finaltouchco-backend.onrender.com"
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "https://finaltouchco-backend.onrender.com"
+// export const BACKEND_URL = "http://localhost:8080";
 
 // filters
 export const EVENT_TYPES = ["Wedding", "Baby Shower", "Engagement", "Bridal Shower", "Birthday"];
